b6989740669e26b72a4f0904b7d6e4934d1d29d0
卢孙远 <476941913@qq.com>
2017-03-31 21:41:06 +0800

    实现了截图的基本功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,57 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_newPushButton_clicked()
+{
+    if(ui->delayRadioButton->isChecked())
+    {
+        qDebug() << "选中了";
+        timer = new QTimer();
+        QObject::connect(this->timer,SIGNAL(timeout()),this,SLOT(screenShotSlot()));
+        this->hide();
+        timer->start(ui->delaySpinBox->text().toInt()*1000);
+    }
+    else
+    {
+        qDebug() << "没有选中";
+        qApp->beep();
+    }
+}
+
+void MainWindow::screenShotSlot()
+{
+
+    qDebug() << "时间到了";
+    this->timer->stop();
+    delete this->timer;
+    this->pixmap = QPixmap::grabWindow(QApplication::desktop()->winId());
+    //scaled按比例缩放
+    ui->preview_label->setPixmap(this->pixmap.scaled(ui->preview_label->size()));
+    this->show();
+
+}
+
+void MainWindow::on_savePushButton_clicked()
+{
+    QString filename = QFileDialog::getSaveFileName(this,"保存截图",QDesktopServices::storageLocation(QStandardPaths::PicturesLocation));
+    if(filename.isEmpty())
+    {
+        QMessageBox::information(this,"保存截图","保存出错");
+        return ;
+    }
+
+    QFile *file=new QFile();
+    bool ok = file->open(filename);
+    if(ok)
+    {
+
+    }
+    else
+    {
+        delete file;
+        return ;
+    }
+    delete file;
+    return ;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QTimer>
+#include <QDesktopWidget>
+#include <QDesktopServices>
+#include <QPixmap>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QDir>
+#include <QString>
+
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +25,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_newPushButton_clicked();
+
+    //截图的槽
+    void screenShotSlot();
+
+    void on_savePushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
+    QPixmap pixmap;
 };
 
 #endif // MAINWINDOW_H
